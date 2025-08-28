4c3d28098895eea82f3aebc0ded4f2e4bea1263f
Aryan Jain <aryanrameshjain@gmail.com>
2025-08-28 16:07:48 +0530

    fix: folder structure after merge
    Signed-off-by: Aryan Jain <aryanrameshjain@gmail.com>

--- src/app/faq/page.js
+++
@@ -1,9 +0,0 @@
-// src/app/faq/page.js
-
-import FaqSection from '../../components/FaqSection';
-
-export default function FaqPage() {
-  return (
-    <FaqSection />
-  );
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import LandingPage from "./components/LandingPage";
 import About from './components/about';
+import FaqSection from "src/components/FaqSection";
 
 export default function Page() {
   return (
+    <div>
     <LandingPage />
       <About />
+      <FaqSection />
+      </div>
   );
 }
